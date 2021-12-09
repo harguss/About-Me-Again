9178c899762201c0e7c03ee77916a6f3275cabb5
SHargus <87950738+harguss@users.noreply.github.com>
2021-12-08 18:37:39 -0600

    connected css and js and added function one

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-use strict
+'use strict';
+console.log('js file connected');
+
+let userName;
+function questionOne(){
+  userName = prompt('What is your name?').toLowerCase();
+  console.log('user input is', userName);
+  alert('Hello ' + userName);
+}
+questionOne();
