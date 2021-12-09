50678cebca1615f40fb6015c5b2962407ef4b346
SHargus <87950738+harguss@users.noreply.github.com>
2021-12-08 20:58:06 -0600

    added second function question

--- js/app.js
+++ js/app.js
@@ -13,15 +13,15 @@ questionOne();
 
 let funFact;
 function functionTwo(){
-  funFact = prompt('Did you watch, I dream of Gennie growing up?')
-console.log('this is their fun fact',funFact);
+  funFact = prompt('Did you watch, I dream of Gennie growing up?');
+  console.log('this is their fun fact',funFact);
 
-if(funFact === 'yes' || funFact === 'YES'){
-  alert('Cool I did too');
-} else if(funFact === 'no' || funFact === 'NO'){
-  alert('You should go watch it');
-} else {
-  alert('Remember to answer yes or no.');
-}
+  if(funFact === 'yes' || funFact === 'YES'){
+    alert('Cool I did too');
+  } else if(funFact === 'no' || funFact === 'NO'){
+    alert('You should go watch it');
+  } else {
+    alert('Remember to answer yes or no.');
+  }
 }
 functionTwo();
